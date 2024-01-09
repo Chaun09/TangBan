9948ce2e1d3ad0cde83c2e69607fe52d4247da53
chaubanban <bangcac24@gmail.com>
2024-01-09 15:35:48 +0700

    upload source code

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'My Love à!',
+    introDesc: `Tức giận sẽ tốn nhiều năng lượng lắm, vì thế đừng giận anh nữa nha baby. Yêu em!`,
+    btnIntro: 'hihi',
+    title: 'Anh không cố ý làm như vậy, anh hứa sẽ sửa sai, không lặp lại nữa. Em tha thứ cho anh nhé!',
+    desc: 'Anh biết lỗi rồi, vì thế bảo bối đừng giận anh nữa nha. Yêu em nhiều!!!',
+    btnYes: 'Vẫn cứ là yêu anh <33',
+    btnNo: 'Không, Anh yêu à :3',
+    question:'Em có muốn quay lại với anh không <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333 em lúc nào cũng yêu anh hết ạ',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Tối nay 8h anh qua đón nhé công chúa.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.facebook.com/manhmeo49' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
