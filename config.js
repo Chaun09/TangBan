09c7841a6bb47a55e19260faae2b85a35f505d16
chouming <bangcac24@gmail.com>
2024-11-23 20:53:40 +0700

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ const CONFIG = {
     btnYes: 'Có <33',
     btnNo: 'Không :<',
     question:'Cho anh 1 cơ hội nha ',
-    btnReply: '',
+    btnReply: 'Em đồng ý đi xem phim với anh nha',
     reply: 'Em đồng ý ạ',
     mess: 'Anh thích em và hy vọng qua web này em có thể biết rằng sẽ luôn có 1 người đợi em, một người luôn sẵn sàng để có thể được chăm sóc , che chở và bảo vệ cho em',
     messDesc: 'Ấn vào đây để nói cho anh biết nhó',
