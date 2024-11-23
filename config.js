77d661e9f611ab3471247d4bf92c43f8d3872d02
chouming <bangcac24@gmail.com>
2024-11-23 21:11:05 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const CONFIG = {
     introTitle: 'Ly à',
     introDesc: `Anh không phải là một người giỏi ăn nói, cũng là một người rất nhạt nhẽo, nhưng anh hiểu đc cảm xúc của em lúc này, em là một cô gái dễ thương và rất tốt, anh thích em. Có lẽ là hơi đường đột để em có thể đón nhận điều này nhưng anh vẫn sẽ thổ lộ vì em xứng đáng nhận được nhiều điều tốt đẹp hơn`,
     btnIntro: '<3',
-    title: 'Em đồng ý đi xem phim với anh nhó',
+    title: 'Gửi đến anh',
     desc: 'Anh thật sự rất thích em và anh hy vọng có thể được bước vào mqh tìm hiểu cùng em',
     btnYes: 'Có <33',
     btnNo: 'Không :<',
